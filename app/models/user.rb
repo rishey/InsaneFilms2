676b4e70b939edef7f63c4c59a92a1106cf895aa
richard bluestein <richard@bluestein.com>
2013-09-20 00:49:20 -0500

    fix layout.css

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,11 @@ require 'bcrypt'
 	has_secure_password
 	has_many	:posts
 	has_many 	:comments, through: :posts
-	# validates :email, presence: true
+	validates :email, presence: true
+	validates :email, uniqueness: true
+	validates :email, format: { with: /\w+@\w+\.\w+/}
+	validates :password_digest, presence: true
+	
 	# validates :email, format: { with: /.*@.*\..*/, message: "invalid email format" }
 	# validates	:password_digest, presence: true
 
